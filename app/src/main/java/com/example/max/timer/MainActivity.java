abb9ed26d9175abd5555a19622072610b70907a4
hu xin <473885523@qq.com>
2018-03-20 22:35:13 +0800

    test

--- app/src/main/java/com/example/max/timer/MainActivity.java
+++ app/src/main/java/com/example/max/timer/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.max.timer;
 
+import android.app.AlertDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView txDate;
 
 
     @Override
@@ -20,20 +28,35 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         final Chronometer time = (Chronometer) findViewById(R.id.chronometer_main);
         Button btnStart = (Button) findViewById(R.id.btn_start);
-
+        txDate = (TextView) findViewById(R.id.tx_date);
 
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
                 time.start();
 
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("设置倒计时")
+                        .setView(buildCalendar())
+                        .show();
+            }
+        });
+
+    }
+
+    CalendarView buildCalendar() {
+        CalendarView calendar = new CalendarView(MainActivity.this);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.i(TAG, "onSelectedDayChange: year " + year + " month " + month + " day " + dayOfMonth);
+                txDate.setText(year+"年"+ month +"月" + dayOfMonth+"日");
 
             }
         });
 
-
+        return calendar;
     }
 }
