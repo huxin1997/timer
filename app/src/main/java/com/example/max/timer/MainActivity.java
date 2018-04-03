3c1cd320d6eb15b3736887e323aab3787408d7e2
贺石骞 <heshiqian3@126.com>
2018-04-03 10:34:58 +0800

    加入swipemenu

--- app/src/main/java/com/example/max/timer/MainActivity.java
+++ app/src/main/java/com/example/max/timer/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.max.timer;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.Build;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +14,19 @@ import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private static final String TAG = "MainActivity";
     private TextView txDate;
+    private CalendarView calendar;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +35,24 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     private void initView() {
         final Chronometer time = (Chronometer) findViewById(R.id.chronometer_main);
         Button btnStart = (Button) findViewById(R.id.btn_start);
         txDate = (TextView) findViewById(R.id.tx_date);
+
+        time.setCountDown(true);
+
+        time.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                String s = chronometer.getText().toString();
+                if ("00:00".equals(s)) {
+                    time.stop();
+                    Toast.makeText(MainActivity.this, "时间到了", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         btnStart.setOnClickListener(new View.OnClickListener() {
@@ -37,21 +60,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                time.start();
-
                 final AlertDialog dateDialog = new AlertDialog.Builder(MainActivity.this)
                         .setTitle("设置倒计时")
                         .setView(buildCalendar())
-                        .setPositiveButton("取消", null)
-                        .setNegativeButton("确认", null)
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                time.setBase(SystemClock.elapsedRealtime());
+                                time.start();
+                            }
+                        })
+                        .setNegativeButton("取消", null)
                         .show();
             }
         });
 
     }
 
-    CalendarView buildCalendar() {
-        CalendarView calendar = new CalendarView(MainActivity.this);
+    private CalendarView buildCalendar() {
+        calendar = new CalendarView(MainActivity.this);
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
